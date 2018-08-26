d9513cc08981b4c432e929f144b7425c0e509f67
DK <dkdan94@gmail.com>
2018-08-25 23:43:18 -0400

    recommendation responsive

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -23,23 +23,25 @@ class Skills extends Component {
           <img className="skill" src="postgreSQL.png" alt="skill" />
         </div>
 
-        <div className="bubbles">
-          <div className="bubble triangle">
-            "I was working / studying with Daniel when we were in Fullstack
-            Academy of Code. Daniel worked hard and always focused on core
-            challenge on each workshop and problem that we encountered...
+        <div className="rec-box">
+          <div className="rec">
+            <img className="recJay" src="jay.jpg" alt="rec" />
+            <div className="bubble triangle">
+              "I was working / studying with Daniel when we were in Fullstack
+              Academy of Code. Daniel worked hard and always focused on core
+              challenge on each workshop and problem that we encountered...
+            </div>
           </div>
-          <div className="bubble triangle">
-            "The first thing you'll probably notice about Dan is that he's a
-            cool guy. He is calm and composed and is a lot of fun to work with.
-            He's always quick to support and treats absolutely everyone with
-            respect and thoughtfulness. Once you get to know him, though, you'll
-            notice more-- Mr. Kim is an...
+
+          <div className="rec">
+            <img className="recKerr" src="kerr.jpg" alt="rec" />
+            <div className="bubble triangle">
+              "The first thing you'll probably notice about Dan is that he's a
+              cool guy. He is calm and composed and is a lot of fun to work
+              with. He's always quick to support and treats absolutely everyone
+              with respect...
+            </div>
           </div>
-        </div>
-        <div className="recBox">
-          <img className="recJay" src="jay.jpg" alt="rec" />
-          <img className="recKerr" src="kerr.jpg" alt="rec" />
         </div>
       </section>
     );
