608e4d074f28b1beb77a86213fe713ec21eacc6a
DK <dkdan94@gmail.com>
2018-09-05 23:39:58 -0400

    media query for timeline

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -24,16 +24,12 @@ class Experience extends Component {
           <li>
             <div className="timeline-panel-left">
               <img src="FSA.png" className="FSA-header" alt="timeline" />
-              {/* <div className="timeline-heading">
-                <h4>FULLSTACK ACADEMY</h4>
-              </div> */}
-              <div className="timeline-body">
-                <p className="timeline-description">
-                  I enrolled in Fullstack Academy to begin my software
-                  engineering career. I gained experience in web development and
-                  in building desktop and mobile applications.
-                </p>
-              </div>
+
+              <p className="timeline-description">
+                I enrolled in Fullstack Academy to begin my software engineering
+                career. I gained experience in web development and in building
+                desktop and mobile applications.
+              </p>
             </div>
           </li>
           <div className="timeline-date">
@@ -42,27 +38,23 @@ class Experience extends Component {
           <li className="timeline-inverted">
             <div className="timeline-panel">
               <img className="PLA-header" src="portnoff.png" alt="timeline" />
-              <div className="timeline-body">
-                <p className="timeline-description">
-                  I accepted my first full-time job as a Legal Assistant in 2016
-                  at Portnoff Law Associates. Here I learned about the nuances
-                  of tax collection in Pennsylvania as I prepared pleadings for
-                  the Firm's attorneys in the collection of delinquent real
-                  estate taxes and municipal fees.
-                </p>
-              </div>
+
+              <p className="timeline-description">
+                I accepted my first full-time job as a Legal Assistant in 2016
+                at Portnoff Law Associates. I prepared pleadings in the
+                collection of delinquent real estate taxes and municipal fees.
+              </p>
             </div>
           </li>
 
           <li>
             <div className="timeline-panel-left">
               <img src="lehigh.jpg" className="Lehigh-header" alt="timeline" />
-              <div className="timeline-body">
-                <p className="timeline-description">
-                  I graduated from Lehigh University in 2016 with a B.A. in
-                  Political Science.
-                </p>
-              </div>
+
+              <p className="timeline-description">
+                I graduated from Lehigh University in 2016 with a B.A. in
+                Political Science.
+              </p>
             </div>
           </li>
           <div className="timeline-date">
@@ -82,13 +74,11 @@ class Experience extends Component {
                 </div>
               </div>
 
-              <div className="timeline-body">
-                <p className="timeline-description">
-                  I interned here under the Honorable Douglas G. Reichley during
-                  my senior year at Lehigh University. I composed summary briefs
-                  for civil cases on tort, contract and property law.
-                </p>
-              </div>
+              <p className="timeline-description">
+                I interned under the Honorable Douglas G. Reichley during my
+                senior year at Lehigh University. I composed summary briefs for
+                civil cases on tort, contract and property law.
+              </p>
             </div>
           </li>
         </ul>
